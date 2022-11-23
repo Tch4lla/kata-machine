445cd1df54cf94f5d57fe4863d6f74105f375722
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-23 22:26:45 +0900

    started to add queue

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,6 +1,12 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
+    private head?: Node<T>;
+    private tail?: Node<T>;
     
 
     constructor() {
