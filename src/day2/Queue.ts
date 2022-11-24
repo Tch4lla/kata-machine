b337b6db213542f94d5f7404d8e1ad8b08deebbd
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-25 07:09:32 +0900

    stack done

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -10,15 +10,38 @@ export default class Queue<T> {
     
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0
     }
 
     enqueue(item: T): void {
+        const node = {value: item} as Node<T>
+        this.length++
+        if(!this.tail){
+            this.tail = this.head = node
+        }
 
-}
+        this.tail.next = node
+        this.tail = node
+    }
+
     deque(): T | undefined {
+        if(!this.head){
+            return undefined
+        }
+        this.length--
 
-}
+        const head = this.head;
+        this.head = this.head.next
+
+        head.next = undefined
+        if(this.length === 0){
+            this.tail = undefined
+        }
+        
+        return head.value
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
